63ad5c95a26fb626a5cbe8babf609333f3850ba3
rahim <rahim.aghareb@gmail.com>
2024-12-28 08:24:56 +0330

    update project settings

---
+++ core/settings/pro.py
@@ -0,0 +1,21 @@
+from .base import *
+
+SECRET_KEY = PRO_SECRET_KEY
+
+DEBUG = False 
+
+ALLOWED_HOSTS = ['hanizstudio.com', 'www.hanizstudio.com']
+
+
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DATABASE_NAME,
+        'USER': DATABASE_USER,
+        'PASSWORD': DATABASE_PASS,
+        'HOST': 'localhost',
+        'PORT': DB_PORT,
+    }
+}
