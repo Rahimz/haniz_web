e8509316a13d1e09d560894c59da004dad2b5836
rahim <rahim.aghareb@gmail.com>
2025-05-18 17:46:38 +0330

    add logger

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -24,3 +24,40 @@ CSRF_TRUSTED_ORIGINS = [
     'https://hanizstudio.com',
     'https://www.hanizstudio.com',
 ]
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'rotating_file': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_errors.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 10,  # Keep 10 backup files
+            'formatter': 'verbose',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['rotating_file', 'console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
