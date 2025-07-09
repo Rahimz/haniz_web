abf0aeaf0e61a01d461487081f9606eb27333662
rahim <rahim.aghareb@gmail.com>
2025-07-09 15:46:20 +0330

    update home page with animated line

--- static/app.js
+++ static/app.js
@@ -1,39 +1,70 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    // for first animations (whithour scrolling)
-    document.getElementById('heroFirstLine').classList.add('heroFirstLine');
-    document.getElementById('heroBox').classList.add('heroBox');
-    document.getElementById('heroMainTitle').classList.add('heroMainTitle');
-    document.getElementById('heroMainTitle').classList.add('heroMainTitle');
-    document.getElementById('heroSubTitle').classList.add('heroSubTitle');
+    // not in mobile devices 
+    var width = window.innerWidth;
+    if (width > 425) {
+        // for first animations (whithour scrolling)
+        document.getElementById('heroFirstLine').classList.add('heroFirstLine');
+        document.getElementById('heroBox').classList.add('heroBox');
+        document.getElementById('heroMainTitle').classList.add('heroMainTitle');
+        document.getElementById('heroSubTitle').classList.add('heroSubTitle');
 
-    // animations depending on scrolling
-    ScrollTrigger.defaults({
-        scrub: 1,
-    });
+        // animations depending on scrolling
+        ScrollTrigger.defaults({
+            scrub: 1,
+        });
 
-    gsap.to('.hero-p', {
-        scrollTrigger: {
-            trigger: '.hero-p',
-            start: '-20 90%',
-            end: '+=80',
-            id: 'hero-p',
-            // markers: true,
-        },
-        opacity: 1,
-        marginTop: '22px',
-    });
+        gsap.to('.hero-p', {
+            scrollTrigger: {
+                trigger: '.hero-p',
+                start: '-20 90%',
+                end: '+=100',
+                id: 'hero-p',
+                // markers: true,
+            },
+            opacity: 1,
+            marginTop: '22px',
+        });
 
-    gsap.to('.heroSecondLine', {
-        scrollTrigger: {
-            trigger: '.hero-p',
-            start: '-20 100%',
-            end: '+=400',
-            // id: '2ndLine',
-            // markers: true,
-        },
-        top: 0,
-        height: '97%',
-    });
+        gsap.to('.heroSecondLine', {
+            scrollTrigger: {
+                trigger: '.hero-p',
+                start: '-20 80%',
+                end: '+=400',
+                // markers: true,
+            },
+            top: -10,
+            height: '100%',
+        });
 
+        gsap.to('.valuesFirstLine', {
+            scrollTrigger: {
+                trigger: '.val-title',
+                start: 'top 85%',
+                end: '+=400',
+                // markers: true,
+            },
+            left: 24,
+        });
+
+        gsap.to('.projectsFirstLine', {
+            scrollTrigger: {
+                trigger: '.pro-title',
+                start: 'top 85%',
+                end: '+=300',
+                // markers: true,
+            },
+            top: 0,
+        });
+
+        gsap.to('.aboutFirstLine', {
+            scrollTrigger: {
+                trigger: '.about',
+                start: 'top 80%',
+                end: '+=400',
+                // markers: true,
+            },
+            left: 20,
+        });
+    }
 });
